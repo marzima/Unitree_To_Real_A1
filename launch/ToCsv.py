3e278f236f17be022c053cfc1578f131418a7fd9
marzima <93491024+marzima@users.noreply.github.com>
2023-09-20 10:57:17 -0400

    Update ToCsv.py

--- launch/ToCsv.py
+++ launch/ToCsv.py
@@ -1,63 +1,24 @@
-import sqlite3
-import csv
-import os
-import sys, getopt
+import subprocess
 
-
-def convert_sqlite_to_csv(inputFolder, ext, tableName):
-    """ inputFolder - Folder where sqlite files are located.
-        ext - Extension of your sqlite file (eg. db, sqlite, sqlite3 etc.)
-        tableName - table name from which you want to select the data.
-    """
-    csvWriter = csv.writer(open(inputFolder + '/output.csv', 'w', newline=''))
-
-    for file1 in os.listdir(inputFolder):
-        if file1.endswith('.' + ext):
-            conn = sqlite3.connect(inputFolder + '/' + file1)
-            cursor = conn.cursor()
-            print(cursor.row_factory)
-            cursor.execute("SELECT * FROM " + tableName)
-            rows = cursor.fetchall()
-            for row in rows:
-                csvWriter.writerow(row)
-            continue
-        else:
-            continue
-
-
-def main(argv):
-    inputFolder = '/home/mistlab/ros2_ws/my_bag1'  # change here
-    extension = 'db3'
-    tableName = 'temperature'  # change here
-
+def launch_ros2_topic_echo(topic_name, output_file):
     try:
-        opts, args = getopt.getopt(argv, "hf:e:t:", ["inputFolder=", "extension=", "tableName="])
-    except getopt.GetoptError:
-        print('multiple_sqlite_files_tocsv.py -d <inputFolder> -e <extension> -t <tableName>')
-        sys.exit(2)
-    for opt, arg in opts:
-        if opt == '-h':
-            print('multiple_sqlite_files_tocsv.py -f <inputFolder> -e <extension> -t <tableName>')
-            sys.exit()
-        elif opt in ("-f", "--inputFolder"):
-            inputFolder = arg
-        elif opt in ("-e", "--extension"):
-            extension = arg
-        elif opt in ("-t", "--tableName"):
-            tableName = arg
+        command = f'ros2 topic echo {topic_name}'
+        process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
 
-    if (len(inputFolder) == 0):
-        print("Please enter inputFolder")
-        print('multiple_sqlite_files_tocsv.py -d <inputFolder> -e <extension> -t <tableName>')
-    elif (len(extension) == 0):
-        print("Please enter extension")
-        print('multiple_sqlite_files_tocsv.py -d <inputFolder> -e <extension> -t <tableName>')
-    elif (len(tableName) == 0):
-        print("Please enter tableName")
-        print('multiple_sqlite_files_tocsv.py -d <inputFolder> -e <extension> -t <tableName>')
-    else:
-        convert_sqlite_to_csv(inputFolder, extension, tableName)
+        with open(output_file, 'w') as file:
+            while True:
+                output = process.stdout.readline()
+                if output == '' and process.poll() is not None:
+                    break
+                if output:
+                    print(output.strip())
+                    file.write(output)
 
+    except KeyboardInterrupt:
+        # Handle user interrupt (Ctrl+C)
+        process.terminate()
 
-if __name__ == "__main__":
-    main(sys.argv[1:])
+if __name__ == '__main__':
+    topic_name = '/imu'  # Replace with the desired topic name
+    output_file = '/home/mistlab/ros2_ws/my_bag/joy_data.txt'  # Specify the output file name here
+    launch_ros2_topic_echo(topic_name, output_file)
